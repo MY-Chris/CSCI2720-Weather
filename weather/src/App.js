adb9124b69e6dfab69f5c45ca2d1be994906c3b8
Sida Huang <45169637+amethystorm@users.noreply.github.com>
2022-05-07 18:05:06 +0800

    router

---
+++ weather/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import MyNavbar from './Components/MyNavbar';
+function App() {
+  return (
+    <div className="App">
+
+        <MyNavbar/>
+
+    </div>
+  );
+}
+
+export default App;
