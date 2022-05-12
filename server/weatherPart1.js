bb0f740d7366331dc29d46c2b9a76a0f507ddeea
SmilyZhang <1155141413@link.cuhk.edu.hk>
2022-05-12 14:47:07 +0800

    Update weatherPart1.js

--- server/weatherPart1.js
+++ server/weatherPart1.js
@@ -11,13 +11,13 @@ const weatherRequest = async (url) => {
 
 // User feature 1, return all locations name lat long and weather info
 const locationsWIthWeather = async function(res){
-    let locations = await schemas.Location.find().select('-_id locName latitude longitude').lean().exec();  
+    let locations = await schemas.Location.find().select('_id locName latitude longitude').lean().exec();  
     console.log("Locations: ");
     console.log( locations);
 
     url = "http://api.weatherapi.com/v1/current.json?key=9035794a7a4444e99da32445220105&q=";
     for (loc of locations){
-        let weather_info = await weatherRequest(url + loc.locName );
+        let weather_info = await weatherRequest(url + loc.locName);
         //axios.get(url + loc.locName).then(res => res.data).catch(error =>{console.log(error);});
         loc.temp_c = weather_info.current.temp_c;
         loc.wind_kph = weather_info.current.wind_kph;
@@ -32,7 +32,7 @@ const locationsWIthWeather = async function(res){
 
 // User feature 2, return all locations name lat long
 const locations = async function(res){
-    let locations = await schemas.Location.find().select('-_id locName latitude longitude').lean().exec();  
+    let locations = await schemas.Location.find().select('_id locName latitude longitude').lean().exec();  
     console.log("Locations: ");
     console.log( locations);
     res.send(locations);
@@ -42,7 +42,7 @@ const searchLocations = async function(res, field, key1, key2 = undefined){
     let locations = [];
     if (key2 == undefined){
         // add partial search
-        locations = await schemas.Location.find({locName: key1}).select('-_id locName latitude longitude').lean().exec();  
+        locations = await schemas.Location.find({locName: key1}).select('_id locName latitude longitude').lean().exec();  
     }else{
         locations = await schemas.Location.find({[field]: { $gte: key1, $lte: key2 } }).select('-_id locName latitude longitude').lean().exec();
     }
@@ -51,8 +51,25 @@ const searchLocations = async function(res, field, key1, key2 = undefined){
     res.send(locations);
 }
 
+// this function is incomplete
+const weatherHistory = async function(res){
+    let locations = await schemas.Location.find({locName: "Beijing"}).select('_id locName latitude longitude').lean().exec();  
+    console.log("Locations: ");
+    console.log( locations);
+
+    url = "http://api.weatherapi.com/v1/history.json?key=9035794a7a4444e99da32445220105&q=";
+    for (loc of locations){
+        let weather_info = await weatherRequest(url + loc.locName + "&dt=2022-05-9&hour=11");
+        //axios.get(url + loc.locName).then(res => res.data).catch(error =>{console.log(error);});
+        loc.weather1 = weather_info
+        loc.weather2 = await weatherRequest(url + loc.locName + "&dt=2022-05-10&hour=11");
+        loc.weather3 = await weatherRequest(url + loc.locName + "&dt=2022-05-11&hour=11");
+    }
+    res.send(locations);
+}
+
 // export function
-module.exports = {locationsWIthWeather, locations, searchLocations};
+module.exports = {locationsWIthWeather, locations, searchLocations, weatherHistory};
 
 
 
